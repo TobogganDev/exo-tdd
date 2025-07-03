3a31e1988c9534a2c24e31d1af45415261cad67d
TobogganDev <doretg.t@gmail.com>
2025-07-03 10:05:01 +0200

    starrt

---
+++ tdd_amplify/TDD_PROJECT/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,21 @@
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "tddproject9fa92b13": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "dynamo58d29e9f": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    }
+  }
+}
